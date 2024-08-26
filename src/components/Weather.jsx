cebcc6fc4ccdede5b5ff0589f56241366894fafd
Techcam1 <167629538+Techcam1@users.noreply.github.com>
2024-08-26 10:28:59 -0400

    added search features

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weather.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -11,6 +11,7 @@ import wind_icon from '../assets/wind.png'
 
 
 const Weather = () => {
+  const inputRef = useRef() 
 
   const [weatherData, setWeatherData] = useState(false);
 
@@ -32,8 +33,12 @@ const Weather = () => {
   }
 
 const search = async (city)=>{
+  if(city === ""){
+    alert("Enter City Name");
+    return;
+  }
   try {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${import.meta.env.VITE_APP_ID}`;
 
     const response = await fetch(url);
     const data = await response.json();
@@ -49,6 +54,8 @@ const search = async (city)=>{
     })
 
   }catch (error) {
+    setWeatherData(False)
+    console.error("Oops, something went wrong");
 
   }
   
@@ -56,17 +63,18 @@ const search = async (city)=>{
 
 
 useEffect(()=>{
-  search("Dallas")
+  search("Washington")
 }
   ,[])
 
   return (
     <div className='weather'>
       <div className="search-bar">
-      <input type="text" placeholder='Search' />
-      <img src={search_icon} alt="" />
+      <input ref={inputRef} type="text" placeholder='Search' />
+      <img src={search_icon} alt="" onClick={()=>search(inputRef.current.value)}/>
       </div>
-      <img src={clear_icon} alt="" className='weather-icon' />
+      {weatherData?<>
+        <img src={weatherData.icon} alt="" className='weather-icon' />
       <p className='temperature'>{weatherData.temperature}°F</p>
       <p className='location'>{weatherData.location}</p>
       <div className="weather-data">
@@ -85,6 +93,8 @@ useEffect(()=>{
           </div>
         </div>
       </div>
+      </>:<></>}
+      
     </div>
   )
 }
